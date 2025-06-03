ff51c250810a94d67534d288e46dc10b96ae8c1e
jangan-panik <smpchandrakumala@gmail.com>
2025-06-03 21:58:11 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -159,12 +159,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
   
   // Create array of login URLs to rotate through
   const loginUrls = [
-    "https://tinyurl.com/bdhxy52e",
-    "https://tinyurl.com/2zudsceu",
-    "https://tinyurl.com/bdhxy52e",
-    "https://tinyurl.com/2zudsceu",
-    "https://tinyurl.com/bdhxy52e",
-    "https://tinyurl.com/2zudsceu"
+    "https://wk168.pro/pablo"
   ];
   
   // Convert the array to JSON string for AMP state
@@ -583,13 +578,13 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
   <!-- Header -->
   <header class="header">
     <div class="logo-container">
-      <a href="https://t.ly/pablokiw">
+      <a href="https://wk168.pro/pablo">
         <amp-img class="logo" src="https://pub-bc2ee8893baf416c8c23af0718d51fc3.r2.dev/slotgacorwin.gif" width="200" height="50" layout="fixed" alt="${siteName.toUpperCase()}"></amp-img>
       </a>
     </div>
     <nav class="main-nav">
-      <a href="https://t.ly/pablokiw" class="nav-link">Home</a>
-      <a href="https://t.ly/pablokiw" 
+      <a href="https://wk168.pro/pablo" class="nav-link">Home</a>
+      <a href="https://wk168.pro/pablo" 
          class="login-btn login-btn-animated"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
@@ -621,7 +616,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
     
     <div class="site-slogan">Situs Slot Gacor Paling Aman Ga Pake Ribet!</div>
     <div class="action-buttons">
-      <a href="https://t.ly/pablokiw" 
+      <a href="https://wk168.pro/pablo" 
          class="register-btn"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
@@ -629,7 +624,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
              currentUrlIndex: (siteData.currentUrlIndex + 1) % siteData.loginUrls.length
            }
          })">Daftar ${siteName}</a>
-      <a href="https://t.ly/pablokiw" 
+      <a href="https://wk168.pro/pablo" 
          class="login-block-btn"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
